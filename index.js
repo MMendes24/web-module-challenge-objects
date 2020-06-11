988f2cf08bbca78ea8bfbf74f74ec89ddbd2c45e
MMendes24 <mmbusiness1@protonmail.com>
2020-06-11 14:11:44 -0400

    Last task finished. Onto stretch.

--- index.js
+++ index.js
@@ -87,11 +87,11 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
-    /* code here */
-  } 
 
-
+function getLastReview(reviews) {
+  return `${reviews[reviews.length -1].name} gave the restaurant a ${reviews[reviews.length -1].rating} star review and their feedback was: ${reviews[reviews.length -1].feedback}`;
+ }
+  
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
 
 /** STRETCH 1: Write a function called `getReviewByRating` that returns an array containing all reviews in a certain range. Your function should accept: 
